5e1634d52dbde754e9301df9a81665b570326b1a
thomas1117 <thomasshannon117@yahoo.com>
2021-01-22 12:22:00 -0800

    update

--- js/operators/atomic.js
+++ js/operators/atomic.js
@@ -1,3 +1,7 @@
+let a = 0
+// set a to the value 1 without changing the line above
+console.assert(a === 1)
+
 const num = 6
 // check that 5 is more than 6 using num with an operator
 // only touch value the variable directly below this comment
@@ -6,5 +10,9 @@ const moreThanFive = null
 console.assert(moreThanFive === true)
 
 let num2 = 7
-// increment num2 by 1 using an operator
+// increment num2 by 1 using an operator (not assignment)
 console.assert(num2 === 8)
+
+let num3 = 6
+// decrease num3 by 1 using an operator (not assignment)
+console.assert(num3 === 5)
