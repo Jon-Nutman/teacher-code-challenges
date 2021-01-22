221b7ded0dd5577159a9bac2edc924a8ca5786de
thomas1117 <thomasshannon117@yahoo.com>
2021-01-22 12:31:37 -0800

    add non-primitives

---
+++ js/operators/atomic/non-primitives.js
@@ -0,0 +1,4 @@
+const arr = [1]
+const firstIsOne = null
+// set firstIsOne to true if the first item is 1
+console.assert(firstIsOne === true)
---
+++ js/operators/atomic/primitives.js
@@ -0,0 +1,41 @@
+let a = 0
+// set a to the value 1 without changing the line above
+console.assert(a === 1)
+
+const num = 6
+// check that 5 is more than 6 using num with an operator
+// only touch value the variable directly below this comment
+const moreThanFive = null
+
+console.assert(moreThanFive === true)
+
+let num2 = 7
+// increment num2 by 1 using an operator (not assignment)
+console.assert(num2 === 8)
+
+let num3 = 6
+// decrease num3 by 1 using an operator (not assignment)
+console.assert(num3 === 5)
+
+let num4 = 7
+// increment num2 by 2 using an operator (not assignment)
+console.assert(num4 === 9)
+
+let num5 = 6
+// decrease num5 by 1 using an operator (not assignment)
+console.assert(num5 === 4)
+
+// string concatenation
+
+let foo = 'hello'
+// using concatenation (+), set foo to hello world
+console.assert(foo === 'hello world!')
+
+let foo2 = 'hello'
+// using concatenation (+=), set foo to hello world
+console.assert(foo2 === 'hello world!')
+
+let foo3 = 'bye'
+let isBye
+// set isBye to true if foo3 is equal to 'bye'
+console.assert(isBye === true)
