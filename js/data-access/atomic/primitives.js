87a0f557cfb06c969d50b978d9ca9528625b4122
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2021-01-25 11:54:40 -0800

    add data access excercises

---
+++ js/data-access/atomic/primitives.js
@@ -0,0 +1,13 @@
+let str1 = "hello";
+//set str1 to the number of characters that the string "str1" has
+let o1;
+console.assert(o1 === 5);
+
+let str2 = "goodbye";
+//set o2 to the first character that the string "str2" has
+let o2;
+console.assert(o2 === "g");
+
+let str3 = "good night";
+//set o3 to the last character that the string "str3" has
+console.assert(o3 === "t");
