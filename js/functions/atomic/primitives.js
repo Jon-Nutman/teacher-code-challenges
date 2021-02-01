0573658598f9d5d2778cb661867a4107d202bc97
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2021-02-01 09:37:02 -0800

    mode functions

--- js/functions/atomic/primitives.js
+++ js/functions/atomic/primitives.js
@@ -1,6 +1,17 @@
 //Q1
-let str1 = "hello";
-//set str1 to the number of characters the variable "str1" has
-let o1;
+//make a function below called "add" that returns the sum
+//of two parameters
 
-console.assert(o1 === 5);
+console.assert(sum(23, 21) === 44);
+
+//Q2
+//make a function below called "minus" that takes two parameters
+//and subtracts the two
+
+console.assert(minus(32, 24) === 8);
+
+//Q3
+//make a function below called "times" that takes two parameters
+//and multiplies them
+
+console.assert(times(3, 5) === 15);
