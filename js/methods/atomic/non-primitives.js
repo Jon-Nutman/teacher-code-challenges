2a267fb45209aa98adb9fe75d664f8b80ff701d2
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2021-02-02 09:12:14 -0800

    start methods exercises

---
+++ js/methods/atomic/non-primitives.js
@@ -0,0 +1,9 @@
+//Q1
+let arr1 = [1, 2, 3];
+let arr1 = [2, 1, 3];
+
+//make a function below called "findOne" that finds the
+//value 1 and returns it and it's index
+
+console.assert(isEmpty(arr1) === true);
+console.assert(isEmpty(arr2) === false);
---
+++ js/methods/atomic/primitives.js
@@ -0,0 +1,5 @@
+//Q1
+//make a function below called "strLength" that returns
+//the length of a string that it receives
+
+console.assert(strLength("apple") === 5);
